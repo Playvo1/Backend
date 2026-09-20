99590c769a7625749ada532008d59a53f09ede16
Hatem Shbair <hatem407910314@gmail.com>
2026-09-20 13:30:09 +0300

    Update status fields in migrations

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
-    Route::post('/register/player', [AuthController::class, 'registerPlayer']);
-    Route::post('/google', [AuthController::class, 'googleAuth']);
-    Route::post('/send-otp', [AuthController::class, 'sendOtp']);
-    Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])
+    Route::post('auth/register/player', [AuthController::class, 'registerPlayer']);
+    Route::post('auth/google', [AuthController::class, 'googleAuth']);
+    Route::post('auth/send-otp', [AuthController::class, 'sendOtp']);
+    Route::post('auth/verify-otp', [AuthController::class, 'verifyOtp']);
+    Route::post('auth/login', [AuthController::class, 'login']);
+    Route::post('auth/logout', [AuthController::class, 'logout'])
         ->middleware('auth:sanctum');
 
-    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-    Route::post('/verify-reset-otp', [AuthController::class, 'verifyResetOtp']);
-    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+    Route::post('auth/forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('auth/verify-reset-otp', [AuthController::class, 'verifyResetOtp']);
+    Route::post('auth/reset-password', [AuthController::class, 'resetPassword']);
 
 
 });
